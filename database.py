1974ec9d9c5a89e69088a1867d3843ff947f7fb0
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2024-09-03 15:59:21 +0300

    Feat: Create a base database module
    - This is also where we get the conn-object from a generator

---
+++ database.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import dotenv
+import logging
+from contextlib import contextmanager
+from typing import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from models import Base
+
+
+log = logging.getLogger(__name__)
+log.basicConfig(level=log.INFO)
+log.info("Database connection established")
+
+dotenv.load_dotenv()
+
+try:
+    SQL_DB_NAME = os.getenv("SQL_DB_NAME")
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///./{SQL_DB_NAME}.sqlite3"
+except Exception as e:
+    log.error(f"Error loading environment variables: {e}")
+    sys.exit(1)
+
+
+# Create the database engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) 
+
+# Create a session object that will be used to interact with the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+@contextmanager
+def get_db() -> Generator[Session, None, None]:
+    """
+    Get database connection object that can be used to interact with the database.
+    It is a generator function that will automatically close the connection after the operation is done.
+    The connection object is used to interact with the database in crud.py.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
